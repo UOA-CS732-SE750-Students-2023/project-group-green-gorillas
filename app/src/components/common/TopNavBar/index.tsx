7f7f756d80094af945adbcb56d916e5760c84d56
Oliver Deng <oliverdeng1005@hotmail.com>
2023-04-27 21:31:59 +1200

    fix screen disapear

--- app/src/components/common/TopNavBar/index.tsx
+++ app/src/components/common/TopNavBar/index.tsx
@@ -14,6 +14,7 @@ import React, { useMemo } from "react";
 import { useCurrentUser } from "../../../hooks/useCurrentUser";
 import { Avatar } from "../Avatar";
 import { useSignOut } from "../../../hooks/useSignOut";
+import { useHistory } from "react-router-dom";
 
 export const TopNavBar = () => {
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
@@ -38,11 +39,17 @@ export const TopNavBar = () => {
 
   const { onSignOut } = useSignOut();
 
+  const { location } = useHistory();
+
+  const shouldShowMenu = useMemo(() => {
+    return location.pathname.startsWith("/main/team");
+  }, [location]);
+
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar
-        position="fixed"
+        position={shouldShowMenu ? "fixed" : "static"}
         sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
       >
         <Toolbar>

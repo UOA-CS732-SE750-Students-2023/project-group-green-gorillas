8b5c573857a05d0bb0a220c3eff44250a48c635c
Monica Zhang <80732580+Monica-Zhang-git@users.noreply.github.com>
2023-04-28 22:45:12 +1200

    Action items (#20)
    * Build team page
    
    * Fetch actionitem list
    
    ---------
    
    Co-authored-by: Oliver Deng <oliverdeng1005@hotmail.com>

--- app/src/api/api.ts
+++ app/src/api/api.ts
@@ -12,3 +12,5 @@ export const RESET_PASSWORD = `${apiBaseUrl}/auth/reset-password`;
 // APIs called by axios
 export const CURRENT_USER = `${apiBaseUrl}/user/current`;
 export const TEAM_BY_ID = (teamId: string) => `${apiBaseUrl}/team/${teamId}`;
+export const ACTIONITEMS_BY_ID = (teamId: string) => `${apiBaseUrl}/action-item/list-outstanding/${teamId}`;
+export const INSIGHT_BY_ID = (teamId: string) => `${apiBaseUrl}/team/insight/${teamId}`;

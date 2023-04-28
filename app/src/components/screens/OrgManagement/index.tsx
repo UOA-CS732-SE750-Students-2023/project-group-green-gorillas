6777bb1ad08596ee8ca323ff13120ec21709a7d4
jennywong1118 <87742159+jennywong1118@users.noreply.github.com>
2023-04-28 22:29:48 +1200

    add LeftNavBar (#19)
    Co-authored-by: Ching Yi Wong <jennywong@Chings-MacBook-Pro.local>

--- app/src/components/screens/OrgManagement/index.tsx
+++ app/src/components/screens/OrgManagement/index.tsx
@@ -1,6 +1,18 @@
 import React, { useEffect } from "react";
 import Org from "./org";
+import { LeftNavBar } from "../../common/LeftNavBar";
+import { Box, Grid } from "@mui/material";
 
 export const OrgManagementScreen = () => {
-  return <div><Org /></div>;
+
+  return (
+    <Grid container>
+      <Grid item xs={2}>
+        <LeftNavBar />
+      </Grid>
+      <Grid item xs={10}>
+        <Org />
+      </Grid>
+    </Grid>
+  );
 };

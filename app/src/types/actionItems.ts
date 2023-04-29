03edb7e3a745cfe7068f43016702366f7ad5bc49
Monica Zhang <80732580+Monica-Zhang-git@users.noreply.github.com>
2023-04-29 19:47:07 +1200

    Render action items (#26)
    * Fix seed data
    
    * fetch actionItems and chart data

--- app/src/types/actionItems.ts
+++ app/src/types/actionItems.ts
@@ -1,3 +1,9 @@
-export type ActionItems = {
-  id: string;
-};
+import {User} from './user';
+
+export type ActionItems = [{
+  id: string,
+  note: string,
+  status: string,
+  createdAt: string,
+  assignees: User[],
+}];

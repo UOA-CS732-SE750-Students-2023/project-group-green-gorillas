313d2beb2af864f5225618b4959c0e829b5698f6
Monica Zhang <80732580+Monica-Zhang-git@users.noreply.github.com>
2023-04-30 17:08:47 +1200

    Update action items status (#31)
    * Fix seed data
    
    * create complete button to update action item status

--- app/src/hooks/useActionItems.ts
+++ app/src/hooks/useActionItems.ts
@@ -1,25 +1,67 @@
 import { useEffect, useState } from "react";
-import { ACTIONITEMS_BY_ID } from "../api/api";
+import { ACTIONITEMS_BY_ID, UPDATE_ACTIONITEMS_BY_ID } from "../api/api";
 import { request } from "../api/request";
-import { ActionItems } from "../types/actionItems";
+// import { ActionItem, Status } from "../types/actionItems";
+import {User} from '../types/user';
+import { typeOf } from "react-is";
+
+
+export enum ActionItemStatus {
+  IN_PROGRESS = "IN_PROGRESS",
+  COMPLETED = "COMPLETED",
+}
+
+export type ActionItem = {
+  id: string,
+  note: string,
+  status: ActionItemStatus,
+  createdAt: string,
+  assignees: User[],
+};
+
 
 export const useActionItems = (id: string) => {
     const[isLoading, setLoading] = useState<Boolean>(false);
-    const [actionItems, setActionItems] = useState<ActionItems | null>(null);
+    const [actionItems, setActionItems] = useState<ActionItem[] | null>(null);
 
-
+    // get action items from backend
     const getActionItems = async () => {
         setLoading(true)
         try{
-            const {data} = await request.get<ActionItems>(ACTIONITEMS_BY_ID(id));
+            const {data} = await request.get<ActionItem[]>(ACTIONITEMS_BY_ID(id));
             setActionItems(data);
         }catch (error) {
             console.log(error); 
         }finally {
             setLoading(false);
         }
+    };
+
+    // update action items to backend
+    const updateActionItems = async (actionItem: ActionItem) => {
+        setLoading(true);
+        const updatedActionItem = {
+            ...actionItem,
+            status: ActionItemStatus.COMPLETED,
+        };
+        setActionItems(actionItems?.map((a) => {
+            return a.id === actionItem.id ? updatedActionItem : a;
+        }) ?? null);
+
+        // const updatedActionItem.status: keyof typeof ActionItemStatus = 'COMPLETED';
+        try{
+            console.log(updatedActionItem.status);
+            
+            await request.patch(UPDATE_ACTIONITEMS_BY_ID, {actionItemId: updatedActionItem.id, status: updatedActionItem.status} );
+        }catch(err){
+            console.log(err);
+        }finally{
+            setLoading(false);
+        }
         
     };
+
+
 
     useEffect(()=>{
         (async () => {
@@ -29,6 +71,7 @@ export const useActionItems = (id: string) => {
 
     return {
         getActionItems,
+        updateActionItems,
         actionItems,
         isLoading,
     };

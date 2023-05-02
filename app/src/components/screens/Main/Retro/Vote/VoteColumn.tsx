9074aa35c855f516452c8eee0a954bc185828612
Oliver Deng <oliverdeng1005@hotmail.com>
2023-05-02 21:28:35 +1200

    add vote feature

--- app/src/components/screens/Main/Retro/Vote/VoteColumn.tsx
+++ app/src/components/screens/Main/Retro/Vote/VoteColumn.tsx
@@ -3,8 +3,21 @@ import VoteGroup from "./VoteGroup";
 import VoteNote from "./VoteNote";
 import stageStyles from "../styles/stage.module.css";
 import { Box } from "@mui/material";
+import { request } from "../../../../../api/request";
+import { UNVOTE_NOTE, VOTE_NOTE } from "../../../../../api/api";
 
 function VoteColumn({ column }: any) {
+  const vote = async (boardNoteId: string, boardId: string) => {
+    await request.post(VOTE_NOTE, {
+      boardNoteId,
+      boardId,
+    });
+  };
+
+  const unvote = async (boardNoteId: string) => {
+    await request.delete(`${UNVOTE_NOTE}/${boardNoteId}`);
+  };
+
   return (
     <Box className={stageStyles.column}>
       <Box className={stageStyles.column__header}>
@@ -21,14 +34,16 @@ function VoteColumn({ column }: any) {
             key={group.id}
             name={group.note}
             items={group.items}
-            votes={10}
-            setVotes={() => {}}
             id={group.id}
+            retroId={group.boardId}
+            votes={group.boardNoteVotes}
+            vote={vote}
+            unvote={unvote}
           />
         );
       })}
       {column.boardNotes.map((item: any) => (
-        <VoteNote note={item} />
+        <VoteNote vote={vote} unvote={unvote} note={item} />
       ))}
     </Box>
   );
--- app/src/components/screens/Main/Retro/Vote/VoteNote.tsx
+++ app/src/components/screens/Main/Retro/Vote/VoteNote.tsx
@@ -1,11 +1,18 @@
 import { Box } from "@mui/material";
-import React from "react";
+import React, { useMemo } from "react";
 
 import voteUp from "../../../../../assets/upvote.svg";
 import voteDown from "../../../../../assets/votedown.svg";
 import stageStyles from "../styles/stage.module.css";
+import { useCurrentUser } from "../../../../../hooks/useCurrentUser";
 
-function VoteNote({ note }: any) {
+function VoteNote({ note, vote, unvote }: any) {
+  const { user } = useCurrentUser();
+
+  const hasVoted = useMemo(() => {
+    return !!note.boardNoteVotes.find((vote: any) => vote.userId === user?.id);
+  }, [note.boardNoteVotes, user]);
+
   return (
     <Box className={stageStyles.note__wrapper} style={{ marginBottom: "16px" }}>
       <Box
@@ -20,30 +27,31 @@ function VoteNote({ note }: any) {
       </Box>
       <Box className={stageStyles.votes__wrapper} style={{ marginTop: "8px" }}>
         <Box className={stageStyles.vote__buttons}>
-          <div className={stageStyles.vote__button}>
-            <Box
-              component="img"
-              src={voteUp}
-              alt=""
-              className={stageStyles.vote__button__img}
-            />
-          </div>
-          <Box
-            className={
-              10 > 0
-                ? stageStyles.vote__button
-                : stageStyles.vote__button__disabled
-            }
-          >
-            <Box
-              component="img"
-              src={voteDown}
-              alt=""
-              className={stageStyles.vote__button__img}
-            />
-          </Box>
+          {!hasVoted ? (
+            <div className={stageStyles.vote__button}>
+              <Box
+                component="img"
+                src={voteUp}
+                onClick={() => vote(note.id, note.boardId)}
+                alt=""
+                className={stageStyles.vote__button__img}
+              />
+            </div>
+          ) : (
+            <Box className={stageStyles.vote__button}>
+              <Box
+                onClick={() => unvote(note.id)}
+                component="img"
+                src={voteDown}
+                alt=""
+                className={stageStyles.vote__button__img}
+              />
+            </Box>
+          )}
         </Box>
-        <Box className={stageStyles.votes}>{10} votes</Box>
+        <Box className={stageStyles.votes}>
+          {note.boardNoteVotes.length} votes
+        </Box>
       </Box>
     </Box>
   );

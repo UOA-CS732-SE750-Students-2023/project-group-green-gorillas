0ddb62d5d4a98b80ec213d2ca35d313243b769df
Monica Zhang <80732580+Monica-Zhang-git@users.noreply.github.com>
2023-05-02 20:33:00 +1200

    Hide button by userTeamRole (#44)

--- app/src/hooks/useTeamRole.ts
+++ app/src/hooks/useTeamRole.ts
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 import { request } from "../api/request";
 import { TEAM_ROLE_BY_ID } from "../api/api";
+import { TeamRole } from "../types/teamRole";
 
 
 export const useTeamRole = (id: string) => {
     const [loading, setLoading] = useState<Boolean>(false);
-    const [teamRole, setTeamRole] = useState();
+    const [teamRole, setTeamRole] = useState<TeamRole | null>(null);
     const getTeamRole =async () => {
         setLoading(true);
         try{
-            const {data} = await request.get(TEAM_ROLE_BY_ID(id));
+            const {data} = await request.get<TeamRole | null>(TEAM_ROLE_BY_ID(id));
+
             setTeamRole(data);
         }catch(err) {
             console.log(err);

3844001c669b510ff15ec7e663650377c4ca2904
Oliver <40448549+dsy401@users.noreply.github.com>
2023-05-02 16:40:22 +1200

    update (#43)

--- app/src/components/screens/Main/Retro/Vote/VoteNote.tsx
+++ app/src/components/screens/Main/Retro/Vote/VoteNote.tsx
@@ -5,7 +5,7 @@ import voteUp from "../../../../../assets/upvote.svg";
 import voteDown from "../../../../../assets/votedown.svg";
 import stageStyles from "../styles/stage.module.css";
 
-function VoteNote({ note, setVotes }: any) {
+function VoteNote({ note }: any) {
   return (
     <Box className={stageStyles.note__wrapper} style={{ marginBottom: "16px" }}>
       <Box
@@ -16,14 +16,11 @@ function VoteNote({ note, setVotes }: any) {
           ] as any
         }
       >
-        {note.value}
+        {note.note}
       </Box>
       <Box className={stageStyles.votes__wrapper} style={{ marginTop: "8px" }}>
         <Box className={stageStyles.vote__buttons}>
-          <div
-            className={stageStyles.vote__button}
-            onClick={() => setVotes("add", note.id)}
-          >
+          <div className={stageStyles.vote__button}>
             <Box
               component="img"
               src={voteUp}
@@ -33,11 +30,10 @@ function VoteNote({ note, setVotes }: any) {
           </div>
           <Box
             className={
-              note.votes > 0
+              10 > 0
                 ? stageStyles.vote__button
                 : stageStyles.vote__button__disabled
             }
-            onClick={() => setVotes("rem", note.id)}
           >
             <Box
               component="img"
@@ -47,7 +43,7 @@ function VoteNote({ note, setVotes }: any) {
             />
           </Box>
         </Box>
-        <Box className={stageStyles.votes}>{note.votes} votes</Box>
+        <Box className={stageStyles.votes}>{10} votes</Box>
       </Box>
     </Box>
   );

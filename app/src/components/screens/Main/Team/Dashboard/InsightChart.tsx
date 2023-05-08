9768dd3b25bab7f2b25536086492a9e2b0ca58c3
Monica Zhang <80732580+Monica-Zhang-git@users.noreply.github.com>
2023-05-08 20:08:27 +1200

    Fixbug (#92)
    * Disable the button for creating retro for the non-team lead user on the template page
    
    * Fix Retro History List Bug
    
    * create pieChart
    
    * update teamplate screen
    
    ---------
    
    Co-authored-by: Monica Zhang <monicazhang@wifi-172-23-1-49.uoa-wifi.auckland.ac.nz>

--- app/src/components/screens/Main/Team/Dashboard/InsightChart.tsx
+++ app/src/components/screens/Main/Team/Dashboard/InsightChart.tsx
@@ -1,10 +1,25 @@
-import { Box, Grid, Paper, Typography } from "@mui/material";
+import { Box, Grid, Paper, Typography, styled } from "@mui/material";
 import { Insight } from "../../../../../types/insights";
+import { PieChart } from "./PieChart";
+
 type Props = {
   insight: Insight | null;
 };
 
 export const InsightChart = ({ insight }: Props) => {
+  const totalCounts: number | null =
+    insight?.outstandingActionItemCount || null;
+  const completedCounts: number | null =
+    insight?.completedActionItemCount || null;
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+
   return (
     <Box
       component="div"
@@ -25,64 +40,60 @@ export const InsightChart = ({ insight }: Props) => {
         Dashboard
       </Typography>
       <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Paper
-            sx={{
-              p: 2,
-              display: "flex",
-              flexDirection: "column",
-              height: 120,
-            }}
-          >
-            {" "}
-            Total Outstanding Action Items
-            <Typography>{insight?.outstandingActionItemCount}</Typography>
-          </Paper>
+        <Grid item xs={7}>
+          <Box>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <Paper sx={{ p: 2 }}>
+                  <Typography
+                    sx={{
+                      color: "#797978",
+                    }}
+                  >
+                    Total Action Items
+                  </Typography>
+                  <Typography variant="h2" sx={{ color: "#1976d2"}}>
+                    {insight?.outstandingActionItemCount}
+                  </Typography>
+                </Paper>
+              </Grid>
+              <Grid item xs={6}>
+                <Paper sx={{ p: 2 }}>
+                  <Typography sx={{ color: "#797978" }}>
+                    Completed Action Items
+                  </Typography>
+                  <Typography variant="h2" sx={{ color: "#1976d2" }}>
+                    {insight?.completedActionItemCount}
+                  </Typography>
+                </Paper>
+              </Grid>
+              <Grid item xs={12}>
+                <Paper sx={{ p: 2 }}>
+                  <Typography sx={{ color: "#797978" }}>
+                    Number of Retros
+                  </Typography>
+                  <Typography variant="h2" sx={{ color: "#1976d2" }}>
+                    {insight?.retrospectiveCount}
+                  </Typography>
+                </Paper>
+              </Grid>
+            </Grid>
+          </Box>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={5}>
           <Paper
             sx={{
               p: 2,
               display: "flex",
               flexDirection: "column",
-              height: 120,
             }}
           >
             {" "}
-            Completed Action Items
-            <Typography>{insight?.completedActionItemCount}</Typography>
-          </Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper
-            sx={{
-              p: 2,
-              display: "flex",
-              flexDirection: "column",
-              height: 120,
-            }}
-          >
-            {" "}
-            Total Action Items
-            <Typography>
-              {insight?.outstandingActionItemCount &&
-                insight?.outstandingActionItemCount +
-                  insight?.completedActionItemCount}
-            </Typography>
-          </Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper
-            sx={{
-              p: 2,
-              display: "flex",
-              flexDirection: "column",
-              height: 120,
-            }}
-          >
-            {" "}
-            Number of Retros
-            <Typography>{insight?.retrospectiveCount}</Typography>
+            Progress of Action Items
+            <PieChart
+              totalCounts={totalCounts}
+              completedCounts={completedCounts}
+            />
           </Paper>
         </Grid>
       </Grid>

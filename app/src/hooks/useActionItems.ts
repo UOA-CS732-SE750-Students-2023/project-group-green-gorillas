5059bf1851940246e0db8512df98821a9fa34282
Oliver <40448549+dsy401@users.noreply.github.com>
2023-05-10 19:10:55 +1200

    Fix action item (#111)

--- app/src/hooks/useActionItems.ts
+++ app/src/hooks/useActionItems.ts
@@ -6,6 +6,7 @@ import {
 } from "../api/api";
 import { request } from "../api/request";
 import { User } from "../types/user";
+import { cloneDeep } from "lodash";
 
 export enum ActionItemStatus {
   IN_PROGRESS = "IN_PROGRESS",
@@ -46,15 +47,21 @@ export const useActionItems = (id: string) => {
       ...actionItem,
       status: ActionItemStatus.COMPLETED,
     };
-    setActionItems(
-      actionItems?.map((a) => {
-        return a.id === actionItem.id ? updatedActionItem : a;
-      }) ?? null
-    );
     try {
+      const updatedActionItem = {
+        ...actionItem,
+        status: ActionItemStatus.COMPLETED,
+      };
       await request.patch(UPDATE_ACTIONITEMS_BY_ID, {
         actionItemId: updatedActionItem.id,
         status: updatedActionItem.status,
+      });
+
+      setActionItems((actionItems) => {
+        if (!actionItems) return actionItems;
+        const clonedActionItems = cloneDeep(actionItems);
+
+        return clonedActionItems.filter((s: any) => s.id !== actionItem.id);
       });
     } catch (err) {
       console.log(err);
@@ -68,7 +75,13 @@ export const useActionItems = (id: string) => {
     setLoading(true);
     try {
       await request.delete(DELETE_ACTIONITEMS_BY_ID(actionItem.id));
-      //   await getActionItems();
+
+      setActionItems((actionItems) => {
+        if (!actionItems) return actionItems;
+        const clonedActionItems = cloneDeep(actionItems);
+
+        return clonedActionItems.filter((s: any) => s.id !== actionItem.id);
+      });
     } catch (err) {
       console.log(err);
     } finally {
@@ -78,9 +91,11 @@ export const useActionItems = (id: string) => {
 
   useEffect(() => {
     (async () => {
-      await getActionItems();
+      if (id) {
+        await getActionItems();
+      }
     })();
-  }, [id, actionItems]);
+  }, [id]);
 
   return {
     getActionItems,
--- app/src/hooks/useRetroHistory.ts
+++ app/src/hooks/useRetroHistory.ts
@@ -4,36 +4,41 @@ import { RETROLIST_BY_ID } from "../api/api";
 import { User } from "../types/user";
 
 export type RetroList = {
-  id: string,
-  name: string,
-  createdAt: string,
-  assignees: User[],
+  id: string;
+  name: string;
+  createdAt: string;
+  assignees: User[];
 };
 
 export const useRetroHistory = (id: string) => {
-    const [loading, setLoadingg] = useState<Boolean>(false);
-    const [retroList, setRetroList] = useState<RetroList[] | null>(null);
+  const [loading, setLoadingg] = useState<Boolean>(false);
+  const [retroList, setRetroList] = useState<RetroList[] | null>(null);
 
-    const getRetroList = async () => {
-        setLoadingg(true);
-        try{
-            const {data} = await request.get<RetroList[] | null >(RETROLIST_BY_ID(id));  
-            // console.log('retro history', data);
-                     
-            setRetroList(data);
-        }catch(error){
-            console.log(error);
-        }finally{setLoadingg(false);}
-    };
-    useEffect(() => {
-        (async ( ) => {
-            await getRetroList();
-        })()
-    }, [id]);
+  const getRetroList = async () => {
+    setLoadingg(true);
+    try {
+      const { data } = await request.get<RetroList[] | null>(
+        RETROLIST_BY_ID(id)
+      );
+      // console.log('retro history', data);
 
-    return{
-        retroList,
-        getRetroList,
-    };
+      setRetroList(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoadingg(false);
+    }
+  };
+  useEffect(() => {
+    (async () => {
+      if (id) {
+        await getRetroList();
+      }
+    })();
+  }, [id]);
 
+  return {
+    retroList,
+    getRetroList,
+  };
 };
